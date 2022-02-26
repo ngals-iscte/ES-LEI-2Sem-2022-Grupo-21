9cf32ef0664556c23da285334ccb150cc138ef4c
ngals-iscte <nathanasmegaslopes@gmail.com>
2022-02-26 16:53:30 +0000

    Alteração no texto

--- ES-Aula02/src/github/Aula02.java
+++ ES-Aula02/src/github/Aula02.java
@@ -2,6 +2,6 @@ package github;
 
 public class Aula02 {
 	  public static void main(String[] args) {
-	        System.out.println("Hello World!"); // Display the string.
+	        System.out.println("I don't want to say hello"); // Display the string.
 	  }
 }
